d7faf160a3dbacdbef0a4d1eac8c3c109ed2ae4c
occn8 <ochenwayward@gmail.com>
2020-05-23 16:02:28 +0300

    file-syts

--- lib/colors/lightblue_grads.dart
+++ lib/colors/lightblue_grads.dart
@@ -1,7 +1,602 @@
 import 'package:flutter/material.dart';
 
+const List<Color> cool_blues = [
+  Color(0xff2193b0),
+  Color(0xff6dd5ed),
+];
+
 const List<Color> moonlit_Astroid = [
   Color(0xff0f2027),
   Color(0xff203a43),
   Color(0xff2c5364),
+];
+
+const List<Color> jshine = [
+  Color(0xff12c2e9),
+  Color(0xffc471ed),
+  Color(0xfff64f59),
+];
+
+const List<Color> cool_sky = [
+  Color(0xff2980b9),
+  Color(0xff6dd5fa),
+  Color(0xffffffff),
+];
+
+const List<Color> memariani = [
+  Color(0xffaa4b6b),
+  Color(0xff6b6b83),
+  Color(0xff3b8d99),
+];
+
+const List<Color> harvey = [
+  Color(0xff1f4037),
+  Color(0xff99f2c8),
+];
+
+const List<Color> azur_lane = [
+  Color(0xff7f7fd5),
+  Color(0xff86a8e7),
+  Color(0xff91eae4),
+];
+
+const List<Color> metapolis = [
+  Color(0xff659999),
+  Color(0xfff4791f),
+];
+
+const List<Color> kye_meh = [
+  Color(0xff8360c3),
+  Color(0xff2ebf91),
+];
+
+const List<Color> by_design = [
+  Color(0xff009fff),
+  Color(0xffec2f4b),
+];
+
+const List<Color> summer_dog = [
+  Color(0xffa8ff78),
+  Color(0xff78ffd6),
+];
+
+const List<Color> blue_raspberry = [
+  Color(0xff00b4db),
+  Color(0xff0083b0),
+];
+
+const List<Color> evening_night = [
+  Color(0xff005aa7),
+  Color(0xfffffde4),
+];
+
+const List<Color> red_sunset = [
+  Color(0xff355c7d),
+  Color(0xff6c5b7b),
+  Color(0xffc06c84),
+];
+
+const List<Color> wedding_day_blues = [
+  Color(0xff40e0d0),
+  Color(0xffff8c00),
+  Color(0xffff0080),
+];
+
+const List<Color> que_pal = [
+  Color(0xff11998e),
+  Color(0xff38ef7d),
+];
+
+const List<Color> pun_yeta = [
+  Color(0xff108dc7),
+  Color(0xffef8e38),
+];
+
+const List<Color> ohhappiness = [
+  Color(0xff00b09b),
+  Color(0xff96c93d),
+];
+
+const List<Color> digital_water = [
+  Color(0xff74ebd5),
+  Color(0xffacb6e5),
+];
+
+const List<Color> hydrogen = [
+  Color(0xff667db6),
+  Color(0xff0082c8),
+  Color(0xff0082c8),
+  Color(0xff667db6),
+];
+
+const List<Color> summer = [
+  Color(0xff22c1c3),
+  Color(0xfffdbb2d),
+];
+
+const List<Color> subu = [
+  Color(0xff0cebeb),
+  Color(0xff20e3b2),
+  Color(0xff29ffc6),
+];
+
+const List<Color> socialive = [
+  Color(0xff06beb6),
+  Color(0xff48b1bf),
+];
+
+const List<Color> telegram = [
+  Color(0xff1c92d2),
+  Color(0xfff2fcfe),
+];
+
+const List<Color> scooter = [
+  Color(0xff36d1dc),
+  Color(0xff5b86e5),
+];
+
+const List<Color> crystal_clear = [
+  Color(0xff159957),
+  Color(0xff155799),
+];
+
+const List<Color> visionof_grandeur = [
+  Color(0xff000046),
+  Color(0xff1cb5e0),
+];
+
+const List<Color> chittychitty_bangbang = [
+  Color(0xff007991),
+  Color(0xff78ffd6),
+];
+
+const List<Color> blue_skies = [
+  Color(0xff56ccf2),
+  Color(0xff2f80ed),
+];
+
+const List<Color> cinnamint = [
+  Color(0xff4ac29a),
+  Color(0xffbdfff3),
+];
+
+const List<Color> maldives = [
+  Color(0xffb2fefa),
+  Color(0xff0ed2f7),
+];
+
+const List<Color> mini = [
+  Color(0xff30e8bf),
+  Color(0xffff8235),
+];
+//star
+const List<Color> pacific_dream = [
+  Color(0xff34e89e),
+  Color(0xff0f3443),
+];
+//star
+const List<Color> orca = [
+  Color(0xff44a08d),
+  Color(0xff093637),
+];
+
+const List<Color> the_blue_lagoon = [
+  Color(0xff43c6ac),
+  Color(0xff191654),
+];
+//star
+const List<Color> under_the_lake = [
+  Color(0xff093028),
+  Color(0xff237a57),
+];
+
+const List<Color> honey_dew = [
+  Color(0xff43c6ac),
+  Color(0xfff8ffae),
+];
+
+const List<Color> cocoaa_ice = [
+  Color(0xffc0c0aa),
+  Color(0xff1cefff),
+];
+
+const List<Color> jodhpur = [
+  Color(0xff9cecfb),
+  Color(0xff65c7f7),
+  Color(0xff0052d4),
+];
+
+const List<Color> vice_city = [
+  Color(0xff3494e6),
+  Color(0xffec6ead),
+];
+
+const List<Color> mild = [
+  Color(0xff67b26f),
+  Color(0xff4ca2cd),
+];
+
+const List<Color> brady_brady_fun_fun = [
+  Color(0xff00c3ff),
+  Color(0xffffff1c),
+];
+
+const List<Color> azure_pop = [
+  Color(0xffef32d9),
+  Color(0xff89fffd),
+];
+
+const List<Color> disco = [
+  Color(0xff4ecdc4),
+  Color(0xff556270),
+];
+
+const List<Color> dania = [
+  Color(0xffbe93c5),
+  Color(0xff7bc6cc),
+];
+
+const List<Color> jupiter = [
+  Color(0xffffd89b),
+  Color(0xff19547b),
+];
+
+const List<Color> iii_delhi = [
+  Color(0xff808080),
+  Color(0xff3fada8),
+];
+
+const List<Color> frost = [
+  Color(0xff000428),
+  Color(0xff004e92),
+];
+
+const List<Color> deep_sea_space = [
+  Color(0xff2c3e50),
+  Color(0xff4ca1af),
+];
+
+const List<Color> sunset = [
+  Color(0xff0b486b),
+  Color(0xfff56217),
+];
+
+const List<Color> solid_vault = [
+  Color(0xff3a7bd5),
+  Color(0xff3a6073),
+];
+
+const List<Color> bright_vault = [
+  Color(0xff00d2ff),
+  Color(0xff928dab),
+];
+
+const List<Color> politics = [
+  Color(0xff2196f3),
+  Color(0xfff44336),
+];
+
+const List<Color> transfile = [
+  Color(0xff16bffd),
+  Color(0xffcb3066),
+];
+
+const List<Color> red_ocean = [
+  Color(0xff1d4350),
+  Color(0xffa43931),
+];
+
+const List<Color> ali = [
+  Color(0xffff4b1f),
+  Color(0xff1fddff),
+];
+
+const List<Color> decent = [
+  Color(0xff4ca1af),
+  Color(0xffc4e0e5),
+];
+
+const List<Color> dark_skies = [
+  Color(0xff4b79a1),
+  Color(0xff283e51),
+];
+
+const List<Color> superman = [
+  Color(0xff0099f7),
+  Color(0xfff11712),
+];
+
+const List<Color> nighthawk = [
+  Color(0xff2980b9),
+  Color(0xff2c3e50),
+];
+
+const List<Color> miami_dolphins = [
+  Color(0xff4da0b0),
+  Color(0xffd39d38),
+];
+
+const List<Color> pizelex = [
+  Color(0xff114357),
+  Color(0xfff29492),
+];
+
+const List<Color> inbox = [
+  Color(0xff457fca),
+  Color(0xff5691c8),
+];
+
+const List<Color> green_and_blue = [
+  Color(0xffc2e59c),
+  Color(0xff64b3f4),
+];
+
+const List<Color> back_to_earth = [
+  Color(0xff00c9ff),
+  Color(0xff92fe9d),
+];
+
+const List<Color> clear_sky = [
+  Color(0xff005c97),
+  Color(0xff363795),
+];
+
+const List<Color> timber = [
+  Color(0xfffc00ff),
+  Color(0xff00dbde),
+];
+
+const List<Color> between_night_and_day = [
+  Color(0xff2c3e50),
+  Color(0xff3498db),
+];
+
+const List<Color> lizard = [
+  Color(0xff304352),
+  Color(0xffd7d2cc),
+];
+
+const List<Color> fresh_turboscent = [
+  Color(0xfff1f2b5),
+  Color(0xff135058),
+];
+
+const List<Color> turquoise_flow = [
+  Color(0xff136a8a),
+  Color(0xff267871),
+];
+
+const List<Color> vine = [
+  Color(0xff00bf8f),
+  Color(0xff001510),
+];
+
+const List<Color> atlas = [
+  Color(0xfffeac5e),
+  Color(0xffc779d0),
+  Color(0xff4bc0c8),
+];
+
+const List<Color> endless_river = [
+  Color(0xff43cea2),
+  Color(0xff185a9d),
+];
+
+const List<Color> purple_bliss = [
+  Color(0xff360033),
+  Color(0xff0b8793),
+];
+
+const List<Color> between_the_clouds = [
+  Color(0xff73c8a9),
+  Color(0xff373b44),
+];
+
+const List<Color> facebook_messeger = [
+  Color(0xff00c6ff),
+  Color(0xff0072ff),
+];
+
+const List<Color> shore = [
+  Color(0xff70e1f5),
+  Color(0xffffd194),
+];
+
+const List<Color> neon_life = [
+  Color(0xffb3ffab),
+  Color(0xff12fff7),
+];
+
+const List<Color> teal_love = [
+  Color(0xffaaffa9),
+  Color(0xff11ffbd),
+];
+
+const List<Color> reef = [
+  Color(0xff00d2ff),
+  Color(0xff3a7bd5),
+];
+
+const List<Color> winter = [
+  Color(0xffe6dada),
+  Color(0xff274046),
+];
+
+const List<Color> forever_lost = [
+  Color(0xff5d4157),
+  Color(0xffa8caba),
+];
+
+const List<Color> moor = [
+  Color(0xff616161),
+  Color(0xff9bc5c3),
+];
+
+const List<Color> aqulicious = [
+  Color(0xff50c9c3),
+  Color(0xff96deda),
+];
+
+const List<Color> miaka = [
+  Color(0xfffc354c),
+  Color(0xff0abfbc),
+];
+
+const List<Color> calm_darya = [
+  Color(0xff5f2c82),
+  Color(0xff49a09d),
+];
+
+const List<Color> stellar = [
+  Color(0xff7474bf),
+  Color(0xff348ac7),
+];
+
+const List<Color> mantle = [
+  Color(0xff24c6dc),
+  Color(0xff514a9d),
+];
+
+const List<Color> opa = [
+  Color(0xff3d7eaa),
+  Color(0xffffe47a),
+];
+
+const List<Color> sea_blizz = [
+  Color(0xff1cd8d2),
+  Color(0xff93edc7),
+];
+
+const List<Color> shroom_haze = [
+  Color(0xff5c258d),
+  Color(0xff4389a2),
+];
+
+const List<Color> moos = [
+  Color(0xff134e5e),
+  Color(0xff71b280),
+];
+
+const List<Color> bora_bora = [
+  Color(0xff2bc0e4),
+  Color(0xffeaecc6),
+];
+
+const List<Color> venice_blue = [
+  Color(0xff085078),
+  Color(0xff85d8ce),
+];
+
+const List<Color> mirage = [
+  Color(0xff16222a),
+  Color(0xff3a6073),
+];
+
+const List<Color> mojito = [
+  Color(0xff1d976c),
+  Color(0xff93f9b9),
+];
+
+const List<Color> sea_weed = [
+  Color(0xff4cb8c4),
+  Color(0xff3cd3ad),
+];
+
+const List<Color> stripe = [
+  Color(0xff1fa2ff),
+  Color(0xff12d8fa),
+  Color(0xffa6ffcb),
+];
+
+const List<Color> aqua_marine = [
+  Color(0xff1a2980),
+  Color(0xff26d0ce),
+];
+
+const List<Color> rose_water = [
+  Color(0xffe55d87),
+  Color(0xff5fc3e4),
+];
+
+const List<Color> emerald_water = [
+  Color(0xff348f50),
+  Color(0xff56b4d3),
+];
+
+const List<Color> green_beach = [
+  Color(0xff02aab0),
+  Color(0xff00cdac),
+];
+
+const List<Color> harmonic_energy = [
+  Color(0xff16a085),
+  Color(0xfff4d03f),
+];
+
+const List<Color> hazel = [
+  Color(0xff77a1d3),
+  Color(0xff79cbca),
+  Color(0xffe684ae),
+];
+
+const List<Color> nimvelo = [
+  Color(0xff314755),
+  Color(0xff26a0da),
+];
+
+const List<Color> sea_blue = [
+  Color(0xff2b5876),
+  Color(0xff4e4376),
+];
+
+const List<Color> sexy_blue = [
+  Color(0xff2193b0),
+  Color(0xff6dd5ed),
+];
+
+const List<Color> skyline = [
+  Color(0xff1488cc),
+  Color(0xff2b32b2),
+];
+
+const List<Color> sel = [
+  Color(0xff00467f),
+  Color(0xffa5cc82),
+];
+
+const List<Color> sky = [
+  Color(0xff076585),
+  Color(0xffffffff),
+];
+
+const List<Color> windy = [
+  Color(0xffacb6e5),
+  Color(0xff86fde8),
+];
+
+const List<Color> bupe = [
+  Color(0xff00416a),
+  Color(0xffe4e5e6),
+];
+
+const List<Color> lunada = [
+  Color(0xff5433ff),
+  Color(0xff20bdff),
+  Color(0xffa5fecb),
+];
+
+const List<Color> anwar = [
+  Color(0xff334d50),
+  Color(0xffcbcaa5),
+];
+
+const List<Color> combi = [
+  Color(0xff00416a),
+  Color(0xff799f0c),
+  Color(0xffffe000),
+];
+
+const List<Color> blu = [
+  Color(0xff00416a),
+  Color(0xffe4e5e6),
 ];
